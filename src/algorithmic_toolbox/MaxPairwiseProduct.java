be2109500cf45903728274d0eae4f0245dfa9eab
Aniket Kumar <aniket24kr@gmail.com>
2021-07-07 16:16:12 +0530

    Create MaxPairwiseProduct.java

---
+++ src/algorithmic_toolbox/MaxPairwiseProduct.java
@@ -0,0 +1,56 @@
+package algorithmic_toolbox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class MaxPairwiseProduct {
+  static long getMaxPairwiseProduct(int[] nums) {
+    Arrays.sort(nums);
+    int len = nums.length;
+    return (long) nums[len - 1] * nums[len - 2];
+  }
+
+  public static void main(String[] args) {
+    FastScanner scanner = new FastScanner(System.in);
+    int n = scanner.nextInt();
+    int[] numbers = new int[n];
+    for (int i = 0; i < n; i++) {
+      numbers[i] = scanner.nextInt();
+    }
+    System.out.println(getMaxPairwiseProduct(numbers));
+  }
+
+  static class FastScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    FastScanner(InputStream stream) {
+      try {
+        br = new BufferedReader(new InputStreamReader(stream));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreTokens()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+  }
+
+}
