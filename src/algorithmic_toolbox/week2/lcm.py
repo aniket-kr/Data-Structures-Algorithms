0af181e336e410a847aed2bf2b79395607c82098
Aniket Kumar <aniket24kr@gmail.com>
2021-07-08 12:10:23 +0530

    Create lcm.py

---
+++ src/algorithmic_toolbox/week2/lcm.py
@@ -0,0 +1,14 @@
+def gcd(a: int , b: int) -> int:
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def lcm(a: int, b: int) -> int:
+    hcf = gcd(a, b) if a > b else gcd(b, a)
+    return (a // hcf) * (b // hcf)
+
+
+if __name__ == '__main__':
+    a, b = [int(x) for x in input().split()]
+    print(lcm(a, b))
